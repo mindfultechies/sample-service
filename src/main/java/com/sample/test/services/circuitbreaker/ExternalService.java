28200ada8b785e2d3123a6cbf45f1afe60b695cb
mindfultechies <160149963+mindfultechies@users.noreply.github.com>
2024-09-16 11:14:12 +0530

    Create ExternalService.java

---
+++ src/main/java/com/sample/test/services/circuitbreaker/ExternalService.java
@@ -0,0 +1,17 @@
+package com.sample.test.services.circuitbreaker
+
+@Service
+public class ExternalService{
+
+    @CircuitBreaker(name = "test", fallbackMethod = "fallback")
+    public ResponseEntity<String> callExternalService(String url)  {
+        throw new RuntimeException("Simulated failure");
+    }
+
+    public ResponseEntity<String> fallback(String url, Throwable throwable) {
+        logger.debug(" service is down. Circuit is open..", throwable);
+        return ResponseEntity.ok("Fallback response");
+    }
+
+  
+}
